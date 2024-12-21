59e5614c6235948b85e68ad309a5002cc57dccec
Clément Noël <noyelclement@hotmail.fr>
2024-12-21 09:24:40 +0100

    feat: add enemy AI and fine tune enemy stats

--- enemy_unit.gd
+++ enemy_unit.gd
@@ -2,4 +2,20 @@ class_name EnemyUnit
 extends Unit
 
 @export var detect_range: float = 100.0
-@onready var gameManager2 = self.gameManager
+
+func _ready() -> void:
+    self.gameManager.enemy_units.append(self)
+
+func _process(_delta: float) -> void:
+    if self.target == null:
+        for player in self.gameManager.player_units:
+            if player == null:
+                continue
+            
+            var distanceBetweenSelfAndTarget = global_position.distance_to(player.global_position)
+            print("Distance between target and enemy :", distanceBetweenSelfAndTarget)
+            if distanceBetweenSelfAndTarget <= detect_range: 
+                set_target(player)
+    
+    target_check()
+      
--- player_unit.gd
+++ player_unit.gd
@@ -3,5 +3,8 @@ extends Unit
 
 @onready var selection_visual: Sprite2D = $SelectionVisual
 
+func _ready() -> void:
+	self.gameManager.player_units.append(self)
+
 func toggle_selection_visual(toggle: bool) -> void:
 	selection_visual.visible = toggle
